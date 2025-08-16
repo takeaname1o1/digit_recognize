1a60c6bebaf2abf533a41acddabb3fb0555c9dbf
JITEN_KUMAR_SHAH_2212070 <38532995+takeaname1o1@users.noreply.github.com>
2025-08-17 00:16:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
-from app import app
+from flask import Flask
 
+# The Flask application object
+app = Flask(__name__)
+
+# Your routes and other application logic go here
+@app.route('/')
+def hello_world():
+    return 'Hello, from your Digit Recognizer!'
+
+# This part is for local development and won't be used by Gunicorn
 if __name__ == '__main__':
-    app.run(host= '0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
